6081d8c0cd4428b234201fb8552e3e332642a9b7
Jakub Zięba <jakubzieba7@gmail.com>
2023-05-21 10:45:42 +0200

    Email parameters

---
+++ WindowsService1/Models/Email.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Models
+{
+    public class Email
+    {
+        private SmtpClient _smtp;
+        private MailMessage _mail;
+
+        private string _hostSmtp;
+        private bool _enableSsl;
+        private int _port;
+        private string _senderEmail;
+        private string _senderEmailPassword;
+        private string _senderName;
+
+        public Email(EmailParams emailParams)
+        {
+            _hostSmtp = emailParams.HostSmtp;
+            _enableSsl = emailParams.EnableSsl;
+            _port = emailParams.Port;
+            _senderEmail = emailParams.SenderEmail;
+            _senderEmailPassword = emailParams.SenderEmailPassword;
+            _senderName = emailParams.SenderName;
+        }
+
+        
+    }
+}
